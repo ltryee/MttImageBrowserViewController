acdadc2d4128e79112cd87730bfbfd89bf7d85d6
vectorliu(刘天扬) <vectorliu@tencent.com>
2015-05-09 16:12:25 +0800

    create project

---
+++ MttImageBrowserViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MttImageBrowserViewController
+//
+//  Created by vectorliu on 15/5/9.
+//  Copyright (c) 2015年 vectorliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MttImageBrowserViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MttImageBrowserViewController
+//
+//  Created by vectorliu on 15/5/9.
+//  Copyright (c) 2015年 vectorliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
